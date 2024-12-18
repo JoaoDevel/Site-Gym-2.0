db3a6b9de9f73a0d4344563691ebd91860ddbfa5
JoãoDevel <joaovitormatosfontes15@gmail.com>
2024-12-18 20:59:15 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./app.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <section className="section first-section">
+        <h1>ola</h1>
+      </section>
+      <section className="section second-section">
+        <h1>ola</h1>
+      </section>
+      <section className="section">
+        <h1>ola</h1>
+      </section>
+    </div>
+  );
+};
+
+export default App;
