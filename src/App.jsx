900b9ce9ba19cf5ee291ba0df6121135c98b2d9a
JoãoDevel <joaovitormatosfontes15@gmail.com>
2024-12-18 21:02:46 +0100

    first commit branch devm

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div className="container">
       <section className="section first-section">
-        <h1>ola</h1>
+        <h1>ola 1</h1>
       </section>
       <section className="section second-section">
         <h1>ola</h1>
