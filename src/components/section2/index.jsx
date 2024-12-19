731df1afc7d12b4cebff662e545a8e1b45ec9cfb
miqueias199808@outlook.com <miqueias199808@outlook.com>
2024-12-20 00:59:41 +0100

    updapte 1.0.3 dvm

--- src/components/section2/index.jsx
+++ src/components/section2/index.jsx
@@ -1,5 +1,7 @@
-import { CircleCheck } from "lucide-react";
+import { Activity, CircleArrowLeft, CircleCheck, Dumbbell } from "lucide-react";
 import "./style.css";
+
+import WomanImage from "../../../src/assets/womangym.png";
 
 const Index = () => {
   return (
@@ -9,6 +11,7 @@ const Index = () => {
         <h1 className="tittle-text">Why gym space</h1>
         <hr className="underline-tittle" />
       </section>
+
       {/**HERE IS THE MAIN-CONTAINER */}
       <main className="main-container">
         <section className="left">
@@ -20,18 +23,60 @@ const Index = () => {
           <hr className="underline-main" />
         </section>
 
-        <article className="center">teste</article>
+        <article className="center">
+          <section className="center-content">
+            <div className="my-fitness">
+              <button>
+                <CircleArrowLeft />
+              </button>
+              <span>MY FITNESS</span>
+            </div>
+            <p className="health">HEALTH</p>
+            <div className="health-content">
+              <span className="porcent">
+                87<span className="porcentN">%</span>
+              </span>
+              <span className="health-icons">
+                <Dumbbell size={19} /> <Activity size={19} color={"black"} />
+              </span>
+            </div>
+            <div className="dates">
+              <div>
+                <a href="">DAILY</a>
+              </div>
+              <div>
+                <a href="">WEAKLY</a>
+              </div>
+              <div>
+                {" "}
+                <a href="">MONTHLY</a>
+              </div>
+            </div>
+            <div className="image">
+              <img src={WomanImage} alt="" />
+            </div>
+          </section>
+        </article>
+
         <section className="right">
-          <span>
-            <CircleCheck /> Hey Noah! You could boost your performance by up to
-            30% every week! Let’s take your eco-conscious
-          </span>{" "}
-          <br />
-          <span>
-            <CircleCheck />
-            Hey Noah! You could boost your performance by up to 30% every week!
-            Let’s take your eco-conscious
-          </span>
+          <section className="right-content">
+            <span>
+              <CircleCheck />
+            </span>
+            <span>
+              Discover how to boost your weekly performance while making
+              eco-friendly choices!
+            </span>
+          </section>
+          <section className="right-content">
+            <span>
+              <CircleCheck />
+            </span>
+            <span>
+              Unlock your potential with simple steps toward sustainability and
+              higher productivity!
+            </span>
+          </section>
         </section>
       </main>
     </section>
